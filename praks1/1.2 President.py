bce6e16a4bdf30490163716907b7dc046b24246e
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-10-05 13:36:46 +0300

    1.1 President

---
+++ praks1/1.2 President.py
@@ -0,0 +1,12 @@
+#defineeri muutujad ja omista väärtused
+aasta = 2016
+president = "Kersti Kaljulaid"
+lause_keskosa = ". valiti Eesti presidentiks "
+
+#Muutujate kasutamine
+lause = str(aasta) + lause_keskosa + president
+print(lause)
+
+
+
+
