1a1b3fbd160879a8f73cab61decc8084f617f2d3
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-10-28 13:52:49 +0300

    2.4 spamm

---
+++ praks1/2.4 spamm.py
@@ -0,0 +1,7 @@
+suurus = input("Sisestage kirja suurus: ")
+teema = input("Sisestage kirja teema pealkiri: ")
+fail = input("Kas kirjaga on kaasas fail? ").lower
+if teema == "" or fail == "jah" and suurus > 1:
+    print("Kiri on spämm")
+else:
+    print("Kiri ei ole spämm")
