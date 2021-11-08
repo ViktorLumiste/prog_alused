b8da4ec8647ff9ed093cfd253dfabbe5102fd669
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-11-08 16:00:22 +0200

    4.2 lipp

--- praks1/4.2 Eesti haldusüksuse lipp.py
+++ praks1/4.2 Eesti haldusüksuse lipp.py
@@ -2,9 +2,9 @@ from tkinter import *
 
 raam = Tk()
 raam.title("Aseri valla lipp")
-tahvel = Canvas(raam, width=600, height = 500)
-tahvel.create_rectangle(0, 0, 600, 200, fill="blue")
-tahvel.create_rectangle(0, 200, 600, 300, fill="white")
-tahvel.create_rectangle(0, 300, 600, 500, fill="green")
+tahvel = Canvas(raam, width=600, height = 400)
+tahvel.create_rectangle(0, 0, 600, 150, fill="dark blue")
+tahvel.create_rectangle(0, 150, 600, 250, fill="white")
+tahvel.create_rectangle(0, 250, 600, 400, fill="green")
 tahvel.pack()
 raam.mainloop()
