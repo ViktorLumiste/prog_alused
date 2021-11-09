0d8b774b360d146f8ddb42fd19060b279726a7c4
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-11-09 14:40:03 +0200

    4.3 Liiklusmark

--- praks1/4.3 Liiklusmark.py
+++ praks1/4.3 Liiklusmark.py
@@ -2,6 +2,6 @@ from tkinter import *
 raam = Tk()
 raam.title("Liiklusmärk 221")
 tahvel = Canvas(raam, width=700, height = 600)
-tahvel.create_polygon(50, 30, 650, 30, 350, 550, fill="white", width = 25, outline="red")
+tahvel.create_polygon(50, 30, 650, 30, 350, 550, fill="white", width = 40, outline="red")
 tahvel.pack()
 raam.mainloop()
