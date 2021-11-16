a3cc4d2c61c5d50f9652b0ba84e920cb00ae71bd
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-11-16 14:35:59 +0200

    5.1 ulikooli vastuvoetud

--- praks1/5.1 ulikooli vastuvoetud.py
+++ praks1/5.1 ulikooli vastuvoetud.py
@@ -1,14 +1,11 @@
 fail = open("rebased.txt", encoding="UTF-8")
 soov = int(input("Palun sisestage, millise aasta andmeid vajate: "))
 
-vastuvõetud = []
 aasta = 2011
 for rida in fail:
-    vastuvõetud.append(int(rida))
     if soov == aasta:
         break
     else:
         aasta += 1
 print(aasta, " aastal oli vastuvõetuid ", rida)
-    
 fail.close()
