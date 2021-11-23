376fa1daa2d8291de9d0e977c455bed1ff07fef6
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-11-23 15:31:51 +0200

    5.6 tahvli juurde

--- praks1/5.6 tahvli juurde.py
+++ praks1/5.6 tahvli juurde.py
@@ -2,7 +2,7 @@ fail = input("Sisestage failinimi: ")
 nimed = open(fail , encoding="UTF-8")
 from datetime import *
 
-nimii = int((datetime.now().day))
+nimii = int(datetime.now().day)
 i = 1
 for nimi in nimed:
     if i == nimii:
