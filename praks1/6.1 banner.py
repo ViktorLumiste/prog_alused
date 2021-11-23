970ae1733d89f198569c9033328cbb1f5efe5267
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-11-23 14:28:29 +0200

    6.1 banner

--- praks1/6.1 banner.py
+++ praks1/6.1 banner.py
@@ -1,10 +1,10 @@
-def banner(lause = input("Sisestage reklaamilause: ")):
+def banner(lause):
     reklaam = lause.upper()
     return(reklaam)
 
 def reklaaam():
     kord = int(input("Mitu korda soovite reklaamlauset kuvada? "))
-    asd = banner()
+    asd = banner(input("Sisestage reklaamilause: "))
     for i in range(1,kord + 1):
         print(asd)
 reklaaam()
