5ef683fd95b2d21709a64d5625989ab2d8543ed1
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-11-30 14:06:39 +0200

    7.2 paevik

--- praks1/7.2 paevik.py
+++ praks1/7.2 paevik.py
@@ -3,7 +3,7 @@ from datetime import *
 paevik = open("paevik.txt" , "a", encoding = "UTF-8")
 kuupaev = datetime.today()
 kirje = input("Sisesta sissekande teksti: ")
-paevik.write(kuupaev + "\n")
+paevik.write("\n" + kuupaev + "\n")
 paevik.write(kirje + "\n")
 
 paevik.close()
