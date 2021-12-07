5b2c1ceb602f20b9696efd78c4a635fc7eac8014
Viktor Lumiste <viktor.lumiste@khk.ee>
2021-12-07 14:58:58 +0200

    7.6 eluteenumber

--- praks1/7.6 Elutee number.py
+++ praks1/7.6 Elutee number.py
@@ -1,21 +1,23 @@
-def elutee(s):
+def elutee(number):
     n = 0
-    for i in s:
-        if i != ".":
-            n += int(i) 
+    for arv in number:
+        if arv != ".":
+            n += int(arv) 
     if n < 10:
         return n
     else:
         return elutee(str(n))
-fail = open("sunnikuupaevad.txt")
 a = 1
 while a <= 9:
-    elutee = open("eluteenumber" + str(a) + ".txt", encoding = "UTF-8")
-    elutee.close()
+    fail1 = open("eluteenumber" + str(a) + ".txt", "x")
+    fail1.close()
+    a += 1
+fail = open("sunnikuupaevad.txt")
 o = 1
 for rida in fail:
-    while elutee(rida) != o:
+    while elutee(rida[:-1]) != o:
         o += 1
     failinimi = "eluteenumber" + str(o)
-    fail1 = open(failinimi + ".txt", "a", encoding = "UTF-8" )
-    fail1.write(rida + "\n")
+    fail2 = open(failinimi + ".txt", "a", encoding = "UTF-8" )
+    fail2.write(rida + "\n")
+    fail2.close()
